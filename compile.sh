0f5a8b7b478cad690cabd6ffc4d878b1b0a1a78f
Damien Cassou <damien.cassou@gmail.com>
2015-01-14 15:18:05 +0100

    Fix compile.sh

--- compile.sh
+++ compile.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
-./pillar export --to="HTML all in one"
-./pillar export --to="Markdown (github) all in one"
-./pillar export --to="LaTeX all in one"
-./pillar export --to="Markdown by chapter"
-./pillar prepareForGitBook --from="Markdown by chapter"
+./pillar export --to="HTML by chapter"
+
