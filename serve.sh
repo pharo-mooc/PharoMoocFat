d1cde28bc7ec9a1e8300ee8e04f5e609c7f36f0c
Luc Fabresse <luc.fabresse@gmail.com>
2015-10-06 12:01:57 +0200

    script to serve html result

--- serve.sh
+++ serve.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-# We need an index.html or a directory listing
 # By Luc 
 
 set -e
@@ -8,5 +7,6 @@ set -e
 PHARO_VM=${PHARO_VM:-./pharo}
 PILLAR_IMAGE=${PILLAR_IMAGE:-./Pillar.image}
 DIRECTORY_TO_SERVE=`pwd`/book-result/
+ST_PATCH_TO_LIST_DIRECTORIES=`pwd`/ZnStaticFileServerDelegate-listDirectories.st
 
-${PHARO_VM} --headless ${PILLAR_IMAGE} eval --no-quit "(ZnServer startDefaultOn: 1701) delegate: (ZnStaticFileServerDelegate new directory: '${DIRECTORY_TO_SERVE}' asFileReference; yourself)"
+${PHARO_VM} --headless ${PILLAR_IMAGE} eval --no-quit "'${ST_PATCH_TO_LIST_DIRECTORIES}' asFileReference fileIn. (ZnServer startDefaultOn: 1701) delegate: (ZnStaticFileServerDelegate new directory: '${DIRECTORY_TO_SERVE}' asFileReference; yourself)"
