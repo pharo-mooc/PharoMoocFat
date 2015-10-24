fbd7ab9b218df21bbb5a0777f1a49d980ad8b3c8
Luc Fabresse <luc.fabresse@gmail.com>
2015-10-24 16:51:34 +0200

    add error messages in compileHandout.sh

--- compileHandout.sh
+++ compileHandout.sh
@@ -63,6 +63,7 @@ for file in $PILLAR_FILES
 do
 	echo
 	./compile.sh --to='BeamerHandout' $file
+	cleanAndExitIfError $? "Pillar compilation of $file FAILED"
 done
 
 #----------------------------------------------
@@ -74,10 +75,12 @@ OTHER_PDFs="Slides/Week1/C019SD-W1S01-ObjectivesMooc.pdf \
 	Slides/Week1/C019SD-W1S03-PharoVision.pdf"
 
 pdfnup --batch --a4paper --keepinfo --nup 2x4 --delta "6mm 6mm" --scale 0.98 --frame true  --no-landscape -o book-result/Slides/Handout/ $OTHER_PDFs 2>&1 1>/dev/null
-
+cleanAndExitIfError $? "Pdfnup of PDF FAILED"
+	
 for file in $OTHER_PDFs
 do	
 	mv -f book-result/Slides/Handout/`basename $file .pdf`-nup.pdf book-result/Slides/Handout/`basename $file` 
+	cleanAndExitIfError $? "copying Handouts FAILED"
 done
 
 #----------------------------------------------
@@ -87,7 +90,7 @@ done
 cp -f Handout/handout.tex book-result/Slides/Handout/
 
 texfot latexmk -pdf -cd book-result/Slides/Handout/handout.tex
-# cleanAndExitIfError $? "Compilation of handout.tex FAILED"
+cleanAndExitIfError $? "Compilation of handout.tex FAILED"
 
 clean
 
